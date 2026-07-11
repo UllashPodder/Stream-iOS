cbf98b863b806d2bb5a37579c020aad85840e835
UllashPodder <ullash.podder@gmail.com>
2026-07-11 12:33:30 +0100

    App Configurations, icon and Primary folder structure

---
+++ Stream-iOS/Stream-iOS/App/Stream_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stream_iOSApp.swift
+//  Stream-iOS
+//
+//  Created by Ullash Podder on 10/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Stream_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
