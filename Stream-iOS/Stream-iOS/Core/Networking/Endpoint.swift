4aa82e43a1268b14c534d9cffebda3a22afa5404
UllashPodder <ullash.podder@gmail.com>
2026-07-15 08:11:15 +0100

    added fix to endpoint component for test

--- Stream-iOS/Stream-iOS/Core/Networking/Endpoint.swift
+++ Stream-iOS/Stream-iOS/Core/Networking/Endpoint.swift
@@ -14,13 +14,13 @@ struct Endpoint{
         var components = URLComponents()
         components.scheme = "https"
         components.host = "rickandmortyapi.com"
-        components.path = "api/" + path
+        components.path = "/api" + path
         components.queryItems = queryItems.isEmpty ? nil : queryItems
         return components.url
     }
 }
 extension Endpoint {
     static func characters(page:Int) -> Endpoint{
-        Endpoint(path: "character", queryItems: [URLQueryItem(name: "page", value: "\(page)")])
+        Endpoint(path: "/character", queryItems: [URLQueryItem(name: "page", value: "\(page)")])
     }
 }
--- Stream-iOS/Stream-iOSTests/EndpointTests.swift
+++ Stream-iOS/Stream-iOSTests/EndpointTests.swift
@@ -9,8 +9,8 @@ import Foundation
 @testable import Stream_iOS
 
 struct EndpointTests {
-    @Test func buildsCharacterPageURL() throws {
-        let url = #require(Endpoint.characters(page: 3).url)
+    @Test func buildsCharacterPageURL() throws {   // <- throws here
+        let url = try #require(Endpoint.characters(page: 3).url)
         #expect(url.absoluteString == "https://rickandmortyapi.com/api/character?page=3")
     }
 }
